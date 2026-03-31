e725a1a45ad226fa435e6e4182e26b7492dfe27c
edisonblack55555 <117357308+edisonblack55555@users.noreply.github.com>
2026-03-31 19:58:06 +0800

    Add files via upload

---
+++ 属性.js
@@ -0,0 +1,26 @@
+var Type_Data={
+    "1": {
+        "Name_cn": "草",
+        "Name": "grass"
+    },
+    "2": {
+        "Name_cn": "水",
+        "Name": "water"
+    },
+    "3": {
+        "Name_cn": "火",
+        "Name": "fire"
+    },
+    "5": {
+        "Name_cn": "电",
+        "Name": "electric"
+    },
+    "7": {
+        "Name_cn": "地面",
+        "Name": "ground"
+    },
+    "999": {
+        "Name_cn": "无",
+        "Name": "none"
+    }
+}
