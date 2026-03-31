e42b2958e0bfc6447e51fe25bf9e92cf798d1760
edisonblack55555 <117357308+edisonblack55555@users.noreply.github.com>
2026-03-31 19:57:19 +0800

    Add files via upload

---
+++ 效果注释.js
@@ -0,0 +1,37 @@
+var Effect_Data={
+    "1": {
+        "Name": "持续生效",
+        "Effect": "在场时会在达成触发条件时反复生效对应的效果。",
+        "Icon": "effectIcon_1"
+    },
+    "2": {
+        "Name": "阵亡",
+        "Effect": "该精灵在战斗中阵亡时触发对应的效果。",
+        "Icon": "effectIcon_2"
+    },
+    "3": {
+        "Name": "连击",
+        "Effect": "可以连续进行两次攻击。",
+        "Icon": "effectIcon_3"
+    },
+    "4": {
+        "Name": "强毒",
+        "Effect": "首次受到其伤害的精灵将被直接消灭。",
+        "Icon": "effectIcon_4"
+    },
+    "5": {
+        "Name": "复活",
+        "Effect": "该精灵阵亡后以特定状态回到场上。",
+        "Icon": "effectIcon_5"
+    },
+    "6": {
+        "Name": "嘲讽",
+        "Effect": "对方精灵必须优先攻击带有嘲讽的精灵。",
+        "Icon": "effectIcon_11"
+    },
+    "7": {
+        "Name": "护盾",
+        "Effect": "可以抵消受到的首次伤害。",
+        "Icon": "effectIcon_12"
+    }
+}
